14206e3827486aadcc96455ecc3a681431810e60
TheRealGerriii <hermanyi-gergely@hotmail.com>
2022-04-06 12:55:40 +0200

    ora eleje

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+use App\Models\School;
+
+
+class StudentController extends Controller
+{
+    // public function students() {
+
+    //     $students = Student::with("course")->get();
+        
+    //     foreach ($students as $student) {
+    //         echo"<pre>";
+    //         print_r( $student->name . " " );
+    //         print_r( $student->course );
+    //     }
+        
+    // }
+    public function students() {
+
+        $schools = School::with("student")->get();
+        
+        foreach ($schools as $school) {
+
+            foreach($school as $sc) {
+                echo"<pre>";
+                print_r($sc);
+            }
+            
+        }
+        
+    }
+}
